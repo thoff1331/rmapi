5dda232158a32b30d523c4ae4654c3733b28f3c5
Trevor hoffman <trevorhoffman@Trevors-MacBook-Air.local>
2019-04-27 19:18:13 -0500

    second

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+
+import './App.css';
+import axios from 'axios';
+
+class App extends Component {
+  constructor(props){
+   super(props);
+
+   this.state = {
+     character: [],
+   }
+  }
+  componentDidMount() {
+   axios.get('https://rickandmortyapi.com/api/character').then(response => {
+     this.setState({character: response.data.results})
+   });
+  };
+
+
+  render() {
+    let map = this.state.character.map(character => {
+    
+      return ( 
+      <div className='fun'>
+      <div>
+      <h1 className='app'>{character.name}</h1> 
+      <img src={character.image} alt='' />
+      </div>
+      </div>
+    
+      )
+    });
+    return (
+      <div className='one'>
+      {map}
+      </div>
+    )
+  }
+}
+
+
+export default App;
